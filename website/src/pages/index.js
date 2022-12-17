c224e91c7f4ac23eb936657cadea35305b32a1f7
dex2dot0 <72662496+dex2dot0@users.noreply.github.com>
2022-12-16 23:26:45 -0600

    PostHog WIP

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,9 +5,15 @@ import Link from '@docusaurus/Link';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import posthog from 'posthog-js'
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const isBrowser = useIsBrowser();
+  if (isBrowser && !window.location.host.includes('localhost')) {
+    posthog.init(siteConfig.postHogApiKey, { api_host: 'https://app.posthog.com' })
+  }
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
